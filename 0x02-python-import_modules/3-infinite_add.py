aec9768c3315e5f0708d4c3203fbe98a6198fe7f
chukssomzzy <chukssomzzy19@gmail.com>
2022-12-04 20:36:43 +0100

    c is fun 🤯

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
-import sys
+from sys import argv, exit
 if __name__ != "__main__":
-    sys.exit()
+    exit()
+arg_len = len(argv)
+sum = 0
+for i in range(1, arg_len):
+    sum += int(argv[i])
+print("{}".format(sum))
 
