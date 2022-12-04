588561a650ed56f9dc64c6242da60c45ebfa823c
chukssomzzy <chukssomzzy19@gmail.com>
2022-12-04 20:03:58 +0100

    dynamic

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) <= 1:
+        print("0 arguments.")
+    elif len(argv) >= 2:
+        argv_len = len(argv)
+        print("{} arguments:".format(argv_len - 1))
+        for i in range(1, argv_len):
+            print("{}: {}".format(i, argv[i]))
