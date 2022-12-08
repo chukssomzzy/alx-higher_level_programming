1a1cb87c1028693fabbcd55424b75a5170f904be
chukssomzzy <chukssomzzy19@gmail.com>
2022-12-08 06:36:14 +0100

    pycodestyle doesn't like lamda function, I guess we are not going serverless soon

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,9 @@
 def square_matrix_simple(matrix=[]):
     """Return the square of a matrix 2d elements
     """
-    ret_red = lambda m: [x ** 2 for x in m]
-    new_matrix = [ret_red(row) for row in matrix]
+    new_matrix = [reduce_mat(row) for row in matrix]
     return (new_matrix)
+
+
+def reduce_mat(matrix=[]):
+    return ([x ** 2 for x in matrix])
