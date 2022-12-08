ae58698e4173a5328c5ee17ac8b2c984de6141b1
chukssomzzy <chukssomzzy19@gmail.com>
2022-12-08 06:27:25 +0100

    this is pretty

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,11 +2,6 @@
 def square_matrix_simple(matrix=[]):
     """Return the square of a matrix 2d elements
     """
-    new_matrix = []
-    for i in range(len(matrix)):
-        tmp_matrix = []
-        for j in range(len(matrix)):
-            tmp_matrix.append(matrix[i][j]**2)
-            if (j == len(matrix[i]) - 1):
-                new_matrix.append(tmp_matrix)
+    ret_red = lambda m: [x ** 2 for x in m]
+    new_matrix = [ret_red(row) for row in matrix]
     return (new_matrix)
