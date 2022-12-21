8aed52451bc828dd8809bb065db1157ccd162af4
chukssomzzy <chukssomzzy19@gmail.com>
2022-12-21 05:30:15 +0100

    not tested

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class square"""
+
+class Square:
+    """Describes the square class"""
+
+    def __init__(self, size):
+        """Initialize a new square obj
+
+        Args:
+            size (int): The size of new square.
+        """
+        self.__size = size
