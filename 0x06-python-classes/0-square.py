d4af8a0ca3f8b155d2ffecb1348c029ae9551fc7
chukssomzzy <chukssomzzy19@gmail.com>
2022-12-21 05:12:28 +0100

    module doc

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""Square intialize a square class
+
+    Examples:
+        from 0-square import Square
+        mysquare = Square()
+"""
+
 class Square:
     """Intialize a square class"""
     pass
