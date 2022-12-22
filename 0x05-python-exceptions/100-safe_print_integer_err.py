f5156c0051770a65563bf70c46d6735b4db98a5b
chukssomzzy <chukssomzzy@gmail.com>
2022-12-22 13:30:11 +0100

    school is not an interger

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,9 +2,17 @@
 import sys
 
 def safe_print_integer_err(value):
+    """Print int while print err to stderr
+
+    Args:
+        value (int): int to print
+
+    Returns:
+        true or False
+    """
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as err:
-        sys.stderr.write("Exception: {}".format(err))
+        sys.stderr.write("Exception: {}\n".format(err))
         return False
     return True
