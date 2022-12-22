56802935f267442fdc9308bddaa3bd6f41ae1c40
chukssomzzy <chukssomzzy19@gmail.com>
2022-12-22 23:06:22 +0100

    bytecode

--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
-safe_function = __import__('101-safe_function').safe_function
+import dis
+safe_function = __import__('102-magic-calculation').magic_calculation
 
+instt = dis.Bytecode(safe_function)
 
-def my_div(a, b):
-    return a / b
-
-result = safe_function(my_div, 10, 2)
-print("result of my_div: {}".format(result))
-
-result = safe_function(my_div, 10, 0)
-print("result of my_div: {}".format(result))
-
-
-def print_list(my_list, len):
-    i = 0
-    while i < len:
-        print(my_list[i])
-        i += 1
-    return len
-
-result = safe_function(print_list, [1, 2, 3, 4], 10)
-print("result of print_list: {}".format(result))
+for inst in instt:
+    print(inst.opname)
