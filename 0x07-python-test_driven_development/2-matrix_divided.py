803043b5cc1d821e095c436c602d5c13faa34fd0
chukssomzzy <chukssomzzy19@gmail.com>
2023-01-04 16:10:57 +0100

    doctest

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Divide each element in a square matrix by a scalar Div """
 
+
 def matrix_divided(matrix, div):
     """Divide each element of a matrix by a scalar
 
@@ -11,32 +12,19 @@ def matrix_divided(matrix, div):
         new_matrix: list of list containing that has been divided by div
     Raises:
         TypeError: when not a matrix or when div is zero or element of matrix
-                    is not a number
+        is not a number
     """
-    if type(div) != int and type(div) != float:
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all(isinstance(elem, float) or isinstance(elem, int) for row in
+                    matrix for elem in row)):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+    if not all(len(matrix[0]) == len(row) for row in matrix):
+        raise TypeError("Each row of the matrix have the same size")
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
-    if div == 0:
+    if not div:
         raise ZeroDivisionError("division by zero")
-    new_matrix = []
-    size = 0
-    if isinstance(matrix, list):
-        inner_mat = []
-        for ilist in matrix:
-            if isinstance(ilist, list):
-                if not size:
-                    size = len(ilist)
-                if size != len(ilist):
-                    raise TypeError("Each row of the matrix must have the same size")
-                for num_int in ilist:
-                    if type(num_int) == int or type(num_int) == float:
-                        inner_mat.append(round(num_int / div, 2))
-                    else:
-                        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-                else:
-                    new_matrix.append(inner_mat)
-                    inner_mat = []
-            else:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        return new_matrix
-    else:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
