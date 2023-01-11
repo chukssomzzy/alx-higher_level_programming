7644a1a48e3db5825f7368cf150dd9502cb86f67
chukssomzzy <chukssomzzy19@gmail.com>
2023-01-12 00:03:56 +0100

    pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,12 +2,15 @@
 
 """Read a file and print to stdout"""
 
+
 def read_file(filename=""):
     """Read content of filename and print output to stdout
 
     Args:
         filename (str): location of file
     """
+    if type(filename) != str:
+        return
     with open(filename, "r", encoding="utf-8") as f:
         for line in f:
             print(line, end="")
