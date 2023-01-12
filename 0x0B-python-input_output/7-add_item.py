7a05ff72b565a16d8aace488dd309a33b39c2609
chukssomzzy <chukssomzzy19@gmail.com>
2023-01-12 03:44:11 +0100

    length

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-
-"""Load serialized data, deserialize the data, sholve them into a
-list and dump them into a file"""
 import sys
 import json
 if __name__ != "__main__":
     exit()
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-
 filename = "add_item.json"
 try:
     items = load_from_json_file(filename)
