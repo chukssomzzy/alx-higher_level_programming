ba73705223c3444e6e3793699e46fbd1eba31998
chukssomzzy <chukssomzzy19@gmail.com>
2023-01-12 03:40:20 +0100

    append use extend

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,22 +9,11 @@ if __name__ != "__main__":
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-list_arg = []
-
-for arg in sys.argv[1:]:
-    list_arg.append(arg)
 
 filename = "add_item.json"
-file_list = []
-
 try:
-    file_list = load_from_json_file(filename)
-    for obj in file_list:
-        list_arg.append(obj)
-except ValueError as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-except FileNotFoundError as e:
-    fp = open(filename, "w", encoding="utf-8")
-    fp.close()
-
-save_to_json_file(list_arg, filename)
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
