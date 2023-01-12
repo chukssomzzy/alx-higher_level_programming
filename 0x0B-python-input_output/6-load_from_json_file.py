13e56d47494a1d7d10a41cc2fc922d5493b29a23
chukssomzzy <chukssomzzy19@gmail.com>
2023-01-12 01:43:49 +0100

    let go deserialization

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Load json from a file"""
+import json
+
+
+def load_from_json_file(filename):
+    """Load json from a file
+
+    Args:
+        filename: filename of  file to deserialize json from
+
+    """
+    if type(filename) != str:
+        return
+    with open(filename, "r+", encoding="utf-8") as f:
+        return json.load(f)
