0ebcf334f27711cfed5d572e2d1dbbae25aa99db
chukssomzzy <chukssomzzy19@gmail.com>
2023-01-12 16:43:32 +0100

    vibe

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""add a method to json"""
+
+
+class Student:
+    """Defines  a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Intialize a student
+
+        Args:
+            first_name:  defines student first first_name
+            last_name: defines student last last_name
+            age: defines age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrievea dict rep"""
+        if not attrs:
+            return self.__dict__
+        return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
