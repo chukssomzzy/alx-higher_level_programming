24e9e2cdfea5f7aca82aaa658b40dfcf8301f6f1
chukssomzzy <chukssomzzy19@gmail.com>
2023-02-18 06:49:02 +0100

    insert

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
 -- print all columns on a database 
-SELECT * from `first_table`;
+SELECT * FROM `first_table`;
--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
---insert a user 
+-- insert a user 
 INSERT INTO `first_table` VALUES (
   89, 
   'Best School'
