5b6ee2c7f35f28bdcd8b9a7f5297aaa118bd58d9
chukssomzzy <chukssomzzy19@gmail.com>
2023-03-13 18:17:56 +0100

    javascript

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const printArr = () => {
+  const arrOfStr = ['C is fun', 'Python is cool', 'javascript is amazing'];
+
+  for (let i = 0; i < arrOfStr.length; i++) {
+    console.log(arrOfStr[i]);
+  }
+};
+
+printArr();
