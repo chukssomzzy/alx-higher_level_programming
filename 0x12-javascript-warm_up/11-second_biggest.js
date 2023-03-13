d05cad3b94ec44b05d2bf236b6cdc0d1e6b46ba9
chukssomzzy <chukssomzzy19@gmail.com>
2023-03-13 22:45:42 +0100

    Remove console.log of argv and adjust comparison logic in secondBiggest function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,9 +3,8 @@
 const secondBiggest = () => {
   const argv = process.argv.slice(2).map(num => Number(num)).filter(num => num);
 
-  console.log(argv);
   if (!argv.length || argv.length === 1) console.log(0);
-  else if (argv.length == 2) console.log(argv[0] > argv[1] ? argv[1] : argv[0]);
+  else if (argv.length === 2) console.log(argv[0] > argv[1] ? argv[1] : argv[0]);
   else console.log((argv.sort())[argv.length - 2]);
 };
 secondBiggest();
