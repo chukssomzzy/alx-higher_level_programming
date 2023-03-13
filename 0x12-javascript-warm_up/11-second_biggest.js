e464d6c9485c7653a409d82843a58d9594a8011c
chukssomzzy <chukssomzzy19@gmail.com>
2023-03-13 22:42:29 +0100

    Update script to filter out empty arguments and log the arguments for debugging

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
 const secondBiggest = () => {
-  const argv = process.argv.slice(2).map(num => Number(num));
+  const argv = process.argv.slice(2).map(num => Number(num)).filter(num => num);
 
+  console.log(argv);
   if (!argv.length || argv.length === 1) console.log(0);
+  else if (argv.length == 2) console.log(argv[0] > argv[1] ? argv[1] : argv[0]);
   else console.log((argv.sort())[argv.length - 2]);
 };
 secondBiggest();
