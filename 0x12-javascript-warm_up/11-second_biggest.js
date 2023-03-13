47c1dff0065ac19bcaf1345e49680b067f396308
chukssomzzy <chukssomzzy19@gmail.com>
2023-03-13 22:00:02 +0100

    Fix secondBiggest function to correctly find second biggest number in array

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ const secondBiggest = () => {
   const argv = process.argv.slice(2).map(num => Number(num));
 
   if (!argv.length || argv.length === 1) console.log(0);
-  else console.log((argv.sort())[1]);
+  else console.log((argv.sort())[argv.length - 2]);
 };
 secondBiggest();
