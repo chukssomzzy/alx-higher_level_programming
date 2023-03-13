c062407ee395c67117b34ac9b013d9bfa5c6d909
chukssomzzy <chukssomzzy19@gmail.com>
2023-03-13 18:21:31 +0100

    Update spelling of "JavaScript" in arrOfStr

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const printArr = () => {
-  const arrOfStr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+  const arrOfStr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
   for (let i = 0; i < arrOfStr.length; i++) {
     console.log(arrOfStr[i]);
