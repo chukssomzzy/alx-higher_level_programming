5c697261f60befcd00215f15f8b56e7ce36ba41f
chukssomzzy <chukssomzzy@gmail.com>
2023-03-16 17:25:29 +0100

    select state and order by id

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python3
 
+"Select states with python from mysql database"
+import sys
+import MySQLdb
+
+username = sys.argv[1]
+password = sys.argv[2]
+db_name = sys.argv[3]
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password,
+                db=db_name, charset="utf8")
+cur = conn.cursor()
+cur.execute(""" SELECT * FROM db_name
+            ORDER BY id """)
