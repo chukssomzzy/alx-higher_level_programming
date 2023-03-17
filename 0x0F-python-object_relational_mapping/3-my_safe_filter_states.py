64bb8a1a1c83930e343281888f5e0ee757d0afb9
chukssomzzy <chukssomzzy19@gmail.com>
2023-03-17 18:33:32 +0100

    Update 3-my_safe_filter_states.py to validate input and add a with statement when executing the SQL query

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,21 +6,18 @@
 import sys
 import MySQLdb
 
-if __name__ != "__main__":
-    sys.exit(1)
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    username = argv[0]
+    password = argv[1]
+    db_name = argv[2]
+    state_name = argv[3]
 
-argv = sys.argv[1:]
-username = argv[0]
-password = argv[1]
-db_name = argv[2]
-state_name = argv[3]
-
-conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                        passwd=password, db=db_name, charset="utf8")
-cur = conn.cursor()
-cur.execute("""SELECT * FROM states WHERE states.name = %s ORDER BY
-            states.id ASC;""", (state_name,))
+    with conn.cursor() as cur:
+        cur.execute("""SELECT * FROM states WHERE states.name = %s ORDER BY
+                    states.id ASC;""", (state_name,))
 
-for row in cur._rows:
-    print(row)
-conn.close()
+        for row in cur:
+            print(row)
