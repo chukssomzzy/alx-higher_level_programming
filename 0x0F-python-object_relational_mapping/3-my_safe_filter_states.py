bec37f6da4e0bf30c74770e546cebd5be9d6f941
chukssomzzy <chukssomzzy19@gmail.com>
2023-03-17 18:39:49 +0100

    Add security to filter states and compile 0-select_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
 """Takes in argument and displays all values in the states table of
-`hbtn_0e_0_usa` where `name` matches the argument"""
+`hbtn_0e_0_usa` where `name` matches the argument
 
+"""
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     argv = sys.argv[1:]
