697541f741320390239da7b4891cc33a86e89263
chukssomzzy <chukssomzzy19@gmail.com>
2023-03-17 18:46:50 +0100

    Update connection parameters for database access

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                       passwd=password, db=db_name, charset="utf8")
+                           passwd=password, db=db_name, charset="utf8")
     with conn.cursor() as cur:
         cur.execute("""SELECT * FROM states ORDER BY states.id ASC;""")
         for row in cur:
