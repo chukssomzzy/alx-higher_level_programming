d5bd57e488621d3ce3b3f648d120be7ba855a38b
chukssomzzy <chukssomzzy19@gmail.com>
2023-03-17 18:26:50 +0100

    Commit message: Update 0-select_states.py and 4-cities_by_state.py to use with-statements and cursor.execute() instead of cur._rows

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,15 +4,14 @@
 import sys
 import MySQLdb
 
-if __name__ != "__main__":
-    sys.exit(1)
-username = sys.argv[1]
-password = sys.argv[2]
-db_name = sys.argv[3]
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                        passwd=password, db=db_name, charset="utf8")
-cur = conn.cursor()
-cur.execute("""SELECT * FROM states ORDER BY states.id ASC;""")
-for row in cur._rows:
-    print(row);
+    with conn.cursor() as cur:
+        cur.execute("""SELECT * FROM states ORDER BY states.id ASC;""")
+        for row in cur:
+            print(row)
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,11 +15,9 @@ db_name = argv[2]
 
 conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                        passwd=password, db=db_name, charset="utf8")
-cur = conn.cursor()
-cur.execute("""SELECT t2.id, t2.name, t1.name
+with conn.cursor() as cur:
+    cur.execute("""SELECT t2.id, t2.name, t1.name
                 FROM states AS t1 INNER JOIN cities AS t2
                 ON t2.state_id = t1.id;""")
-
-for row in cur._rows:
-    print(row)
-conn.close()
+    for row in cur:
+        print(row)
