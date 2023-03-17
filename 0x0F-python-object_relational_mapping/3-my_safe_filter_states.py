81c346b26ddaada760114cfc7774c2a486ab56b9
chukssomzzy <chukssomzzy19@gmail.com>
2023-03-17 19:00:34 +0100

    Update 1-filter_states.py, 3-my_safe_filter_states.py, and 0-select_states.cpython-311.pyc files

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,7 @@ import MySQLdb
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                       passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     with conn.cursor() as cur:
         cur.execute("""SELECT * FROM states WHERE states.name = %s ORDER BY
                     states.id ASC;""", (sys.argv[4],))
