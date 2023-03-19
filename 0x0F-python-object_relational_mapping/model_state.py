c832dd4b2715567186c2258197d72292f8a86f60
chukssomzzy <chukssomzzy@gmail.com>
2023-03-19 16:42:52 +0100

    linted with flake*

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,9 +5,12 @@ declarative()"""
 from sqlalchemy import Column, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
-Base = declarative_base();
+
 class State(Base):
+    """Defines a base class states"""
+
     __tablename__ = "states"
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     name = Column(String(128), nullable=False)
