9a0bad4d6a4c94ead659498841c16903cf319b82
chukssomzzy <chukssomzzy@gmail.com>
2023-03-20 14:45:49 +0100

    state.cities

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     for state, id, city in session.query(State.name, City.id, City.name).\
-            join(City, State.id == City.state_id).all():
+            join(City, State.cities).all():
         print(state, id, city)
