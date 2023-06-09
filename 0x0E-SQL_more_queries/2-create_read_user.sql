ede29ca4efa7af32a0a22e78b22957a8304a198b
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-09 15:48:45 +0000

    wrong pass

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,7 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 
 -- create user 
-CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY 'user_0d_2_p@WD';
+CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY `user_0d_2_pwd`;
 
 -- grant read access 
 GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`; 
