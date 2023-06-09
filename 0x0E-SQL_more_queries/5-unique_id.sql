66754c764215f6429e22b35076c0fda72747714d
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-09 16:06:57 +0000

    style

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,7 @@
 -- CREATE A sql statement with unique ID 
 
 -- create Table 
-CREATE TABLE IF NOT EXISTS `unique_id` (
- id INT UNIQUE DEFAULT 1,
-name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS `unique_id`(
+  id INT UNIQUE DEFAULT 1,
+  name VARCHAR(256)
 )
