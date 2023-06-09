1832155c1bfd0c6289ccdef58ffadde51bfbe5ad
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-09 15:35:07 +0000

    grant

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,4 +4,7 @@
 CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY 'user_0d_1_pwd';
 
 -- GRANT priviledges 
-GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost` WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON  *.* TO `user_0d_1`@`localhost`;
+
+-- flush priv 
+FLUSH PRIVILEGES;
