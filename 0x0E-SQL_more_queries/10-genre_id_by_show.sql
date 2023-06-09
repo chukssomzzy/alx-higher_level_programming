691134bf9dc9886eb4600fbaeced3ae79afa5268
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-09 22:31:37 +0000

    joint

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- select by genre and show 
+
+-- SELECT and inner join 
+SELECT tv_show.title, tv_show_genres.genre_id FROM tv_shows 
+  INNER JOIN tv_show_genres ON
+    tv_show.id = tv_show_genres.show_id 
+      ORDER BY tv_shows.title, tv_show_genres.genre_id;
