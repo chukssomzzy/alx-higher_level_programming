ce622ba03312e19a68f93f3313eb8697dbebeba5
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-09 17:37:45 +0000

    join inner

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list cities.id, cities.name, states.name 
 
---cities states
+-- cities states
 SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON 
   cities.state_id = states.id ORDER BY cities.id ASC;
