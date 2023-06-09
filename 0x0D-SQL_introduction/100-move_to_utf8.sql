9e06a6160738bcc02f8bf5c4867bc58008cdc7d3
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-09 00:24:03 +0000

    end

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,17 +1,13 @@
 -- Alter Database, Table and Colmn collate attribute
-
 -- db 
 USE hbtn_0c_0;
-
 -- Change Database Collate 
 ALTER DATABASE hbtn_0c_0
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
-
+    CHARACTER SET UTF8MB4
+    COLLATE UTF8MB4_UNICODE_CI;
 -- Alter table character set and collate 
 ALTER TABLE first_table 
-  CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+  CONVERT TO CHARACTER SET UTF8MB4 COLLATE UTF8MB4_UNICODE_CI;
 -- Alter column 
 ALTER TABLE first_table 
-  MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+  MODIFY name VARCHAR(256) COLLATE UTF8MB4_UNICODE_CI;
