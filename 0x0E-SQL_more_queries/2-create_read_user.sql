b9438863bf07d6d28cb8b168aad706cb5d88c344
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-09 15:46:56 +0000

    always remember test

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,10 +4,10 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 
 -- create user 
-CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY 'user_0d_2_p@WD';
 
 -- grant read access 
-GRANT SELECT PRIVILEGES ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`; 
+GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`; 
 
 -- flush privileges 
 FLUSH PRIVILEGES;
