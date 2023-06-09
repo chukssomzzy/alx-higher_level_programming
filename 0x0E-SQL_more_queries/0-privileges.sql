7f2258d24f9262f8da5c85c659fe562188539af0
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-09 15:14:48 +0000

    priviledges granted

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- list all privileges of the MySQL users user_0d_1 and user_0d_2 on your server in localhost 
+
+-- show grant for user_0D_1 
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- show grant for user_0d_2 
+SHOW GRANTS FOR `user_0d_2`@`localhost`;
