03ae4235413348835283aa420f7a21356d0dbe27
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-09 15:42:56 +0000

    not tested

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,13 @@
+-- Create user with only read privileges 
+
+-- creates database
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+
+-- create user 
+CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY 'user_0d_2_pwd';
+
+-- grant read access 
+GRANT SELECT PRIVILEGES ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`; 
+
+-- flush privileges 
+FLUSH PRIVILEGES;
