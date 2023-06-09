a9f86fecc156df82ffd41399836564b5089f7f63
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-09 01:22:33 +0000

    avg

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- top city 
+
+-- top city from the month of july and august 
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+  WHERE month = 7 OR month = 8
+  GROUP BY city 
+  ORDER BY avg_temp 
+  LIMIT 3;
