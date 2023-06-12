b4b9f0c7cb1c70f958e4b31c57734c0a88afeb18
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-12 06:52:55 +0100

    limit next

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,8 +1,16 @@
 -- Left join 
 
 -- query with left join and , 
-
-SELECT DISTINCT tv_shows.title FROM tv_shows LEFT OUTER JOIN (tv_genres, tv_show_genres)
-  ON (tv_shows.id = tv_show_genres.show_id AND tv_genres.id = tv_show_genres.genre_id)
-  WHERE tv_genres.name <> 'Comedy' OR tv_genres.id IS NULL
-  ORDER BY tv_shows.title ASC;
+SELECT DISTINCT tv_shows.title FROM tv_shows
+  LEFT JOIN 
+    tv_show_genres
+  ON 
+    (tv_shows.id = tv_show_genres.show_id)
+  LEFT JOIN 
+    tv_genres 
+  ON 
+    (tv_genres.id = tv_show_genres.genre_id)
+  WHERE 
+    tv_genres.name <> 'Comedy' OR tv_genres.id IS NULL
+  ORDER BY 
+    tv_shows.title ASC;
