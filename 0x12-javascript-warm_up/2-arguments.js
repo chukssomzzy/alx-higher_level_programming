13ce8bf1824e4424f09db1ba7029226733f7865b
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-13 23:59:24 +0100

    bis:

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node
 
+const argv_v = process.argv;
+
+if (argv_v) { console.log('Argument found'); } else if (argv_v.length > 3) { console.log('Arguments found'); } else { console.log('No argument'); }
