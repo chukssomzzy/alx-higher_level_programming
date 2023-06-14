4a2b894f1ab80f02ef8e809c2a5b99032630e124
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-14 01:05:31 +0100

    0

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,12 +4,13 @@ const printSquare = (size) => {
   let charC = '';
 
   size = Number(size);
-  if (size) {
+  if (size || size === 0) {
     for (let i = 0; i < size; i++) {
       for (let j = 0; j < size; j++) { charC += '#'; }
       if (i < size - 1) { charC += '\n'; }
     }
-  } else { return ('Missing size'); }
-  return (charC);
+    return (charC);
+  }
+  return ('Missing size');
 };
 console.log(printSquare(process.argv[2]));
