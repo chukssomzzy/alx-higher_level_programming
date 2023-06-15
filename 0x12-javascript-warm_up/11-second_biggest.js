8113db564b4c79f6ec92797ad0ba4e37d9142a49
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-15 09:26:13 +0100

    second largest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const list = process.argv.splice(2);
+
+const secondLargest = (list) => {
+  if (!list.length || list.length === 1) { return (0); }
+  const sortedList = list.sort((a, b) => (b - a));
+  console.log(sortedList);
+  return (sortedList[1]);
+};
+
+console.log(secondLargest(list));
