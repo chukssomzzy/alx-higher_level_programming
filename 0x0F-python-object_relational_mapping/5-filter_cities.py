8225684f337214c5f4afe788ae6b5b9f9b06b075
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-17 20:12:57 +0000

    Tweak

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Takes in the name of a state as an argument and lists all `cities` of that
+    state, using the database `hbtn_0e_0_usa`
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    with MySQLdb.connect(host="localhost", passwd=sys.argv[2], port=3306,
+                         user=sys.argv[1], db=sys.argv[3]) as conn:
+        with conn.cursor() as cur:
+            cur.execute("""SELECT cities.name FROM cities
+                            INNER JOIN
+                                states
+                            ON
+                                (states.id = cities.state_id)
+                            WHERE
+                                states.name = %s""", (sys.argv[4],))
+            cities = cur.fetchall()
+            for i in range(len(cities)):
+                print(cities[i][0], end='')
+                if i < (len(cities) - 1):
+                    print(', ', end='')
+            else:
+                print()
