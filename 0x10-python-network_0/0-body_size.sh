0f92aa390bc86c72e2a59226ef3576e715d9fce2
chukssomzzy <chukssomzzy19@gmail.com>
2023-06-29 16:40:38 +0000

    first line

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash 
+#!/bin/bash 
 # takes a URL, sends a request to that URL and displays the size of the body of the responds 
 curl -s --head $1 | grep Content-Length | awk '{ print $2; }'
