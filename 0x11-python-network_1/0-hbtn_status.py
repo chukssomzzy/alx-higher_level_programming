8cc0933927ac99be0cebd8af451cd53fb5ca1185
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-01 17:54:29 +0000

    hopes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,4 +9,4 @@ with urlopen(url_request) as r:
     con_str = r.read()
     print(
         f"Body response:\n\t- type: {type(con_str)}\n\t- content: {con_str}\
-        \n\t- utf8 content: {str(object=con_str, encoding='utf-8')}\n")
+\n\t- utf8 content: {str(object=con_str, encoding='utf-8')}")
