4d041d42c0ee78b3ce6cab496ca984a61997e3e1
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-02 09:44:23 +0100

    not a valid json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Takes in a letter and sends a POST request to
+`http://0.0.0.0:5000/search_user` with the letter as
+params
+"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    q = len(argv) == 2 and argv[1] or ""
+    r = requests.post("http://0.0.0.0:5000/search_user", params={'q': q})
+    print(r.json)
+    if r.json():
+        print(f"[{r.json().get('id')}] {r.json().get('name')}")
