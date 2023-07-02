a310621cb2f2688bc858910ce8fecaebee48ea2c
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-02 01:57:58 +0000

    r an

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Takes in URL and an email address, sends a POST request to the passed
+URL with the email as a parameter and finally displays the response
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.post(argv[1], params={"email": argv[2]})
+    print(r.text)
