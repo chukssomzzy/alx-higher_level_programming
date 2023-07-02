fe47fe5129adaaa77cb3dee1f7ee21bcff54eb9c
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-02 03:01:10 +0000

    no warns

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""takes in a URL, sends a request to the URL and displays the body of the
+response"""
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    if r.raise_for_status:
+        print(f"Error code: {r.status_code}")
+        exit(1)
+    else:
+        print(r.text)
