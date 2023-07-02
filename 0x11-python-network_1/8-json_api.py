1786ddf0d36b22848acbd500b7dd90b1db5ff6c1
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-02 09:59:21 +0100

    data

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ from sys import argv
 
 if __name__ == '__main__':
     q = len(argv) == 2 and argv[1] or ""
-    r = requests.post("http://0.0.0.0:5000/search_user", params={'q': q})
+    r = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
     if r.json():
         print(f"[{r.json().get('id')}] {r.json().get('name')}")
     elif len(r.json()) == 0:
