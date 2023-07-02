3035b27d5884b43cbda38d2c04c1f9c6f4c78589
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-02 11:35:06 +0000

    headeer

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Takes your GitHub credentials (username and password) and uses the
+Github api to display your id
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    header = {"Authorization":
+              "Bearer github_pat_11AQN7SBY0T0wxXlTXOvyy_ZZMLc8OMSxmVSvue2FVuKS\
+sZphr2Iim1e8yahwBoetKQLDCE2XLo1piXLbB",
+              "X-Github-Api-Version": "2022-11-28"
+              "Accept": "application/vnd.github+json"}
+    r = request.get(f"https://api.github.com/user/{argv[1]}", header=header)
