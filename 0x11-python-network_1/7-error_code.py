225aed446342049f8630f566db8405678c19725f
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-02 05:15:43 +0100

    raise status

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get(argv[1])
-    if r.raise_for_status:
+    if r.raise_for_status():
         print(f"Error code: {r.status_code}")
     else:
         print(r.text)
