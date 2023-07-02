fca3c4d7d2750d865bc8a534ee43183c717b2862
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-02 01:48:22 +0000

    hmm

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,9 +4,9 @@
 variable `X-Request-Id` in the response header
 """
 import requests
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
-    r = requests.get(argv[1])
+    r = requests.get(sys.argv[1])
     print(r.headers['X-Request-Id'])
