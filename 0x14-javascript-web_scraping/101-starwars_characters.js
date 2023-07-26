7a63757945e143b453b6eebdca6ba3d2ed92dac0
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-26 14:46:28 +0100

    slow bit works

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,21 +2,29 @@
 
 const request = require('request');
 try {
-  const url = `https://swapi-api.alx-tools.com/api/films/${Number(process.argv[2])}/`;
+  const url = `https://swapi-api.alx-tools.com/api/films/${Number(process.argv[2])}`;
   request(url, (err, res, body) => {
     if (err) throw err;
 
     const filmData = JSON.parse(body);
     const characterUrls = filmData.characters;
-    for (let i = 0; i < characterUrls.length; i++) {
-      request(characterUrls[i], (err, res, body) => {
-        if (err) throw err;
-
-        const data = JSON.parse(body);
-        console.log(data.name);
-      });
-    }
+    recursivePrint(characterUrls);
   });
 } catch (e) {
   console.log(e);
 }
+
+const recursivePrint = async (characterUrls, index = 0) => {
+  try {
+    if (index === characterUrls.length) { return; }
+    request(characterUrls[index], (err, resp, body) => {
+      if (err) throw err;
+
+      const characterName = JSON.parse(body).name;
+      console.log(characterName);
+      recursivePrint(characterUrls, index + 1);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
