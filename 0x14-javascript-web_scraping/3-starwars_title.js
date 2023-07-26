88308fe25d34baef0c4e9ec295fade5b64707ca5
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-26 04:57:00 +0000

    a new hope

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,4 +3,7 @@
 const request = require("request")
 
 url = `https://swapi-api.alx-tools.com/api/films/${Number(process.argv[2])}`
-request(url, (err, res, body) => console.log(body.title))
+request(url, (err, res, body) => {
+    data = JSON.parse(body)
+    console.log(data.title)
+})
