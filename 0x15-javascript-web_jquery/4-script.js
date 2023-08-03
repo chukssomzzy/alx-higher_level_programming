6e534a552a6b44981dc6aefe1821ea30acf1e273
chukssomzzy <chukssomzzy19@gmail.com>
2023-08-03 00:26:23 +0000

    toggle_header

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+$('DIV#toggle_header').bind('click', () => {
+  const header = $('header');
+  if (header.hasClass('red')) {
+    header.removeClass('red');
+    header.addClass('green');
+  } else if (header.hasClass('green')) {
+    header.removeClass('green');
+    header.addClass('red');
+  }
+});
